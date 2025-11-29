34be3664004dd265ff70fd27b7f5a4d3ca3e0f4f
Remy <segunismail1234@gmail.com>
2025-11-29 10:49:23 +0100

    Remove the date field in prisma

--- app/api/events/[slug]/route.ts
+++ app/api/events/[slug]/route.ts
@@ -3,21 +3,30 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 export async function GET(request: Request, { params }: { params: { slug: string } }) { 
     try {
-        const { slug } = await params;
+      const { slug } = await params;
 
-       const refineSlug = slug.toLowerCase().trim()
+      const refineSlug = slug.toLowerCase().trim();
 
-        const event =  await prisma.event.findUnique({
-            where: { slug: refineSlug }
-        })
+      const event = await prisma.event.findUnique({
+        where: { slug: refineSlug },
+      });
 
-        if (!event) {
-            return new Response('Event Not Found', { status: 404 });
-        }
+      if (!event) {
+        return NextResponse.json("Event Not Found", { status: 404 });
+      }
 
-        return NextResponse.json({message: 'Event fetched successfully', data: event}, {status: 200});
+      return NextResponse.json(
+        { message: "Event fetched successfully", data: event },
+        { status: 200 },
+      );
     }
     catch (error) { 
-        return NextResponse.json({message: 'Internal Server Error'}, { status: 500 });
+        return NextResponse.json(
+          {
+            message: "Internal Server Error",
+            error: error instanceof Error ? error.message : "Unknown",
+          },
+          { status: 500 },
+        );
     }
 }
